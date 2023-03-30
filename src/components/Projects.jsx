940a87b9e574754d00b8cc5a3e321e04c1b1f725
francisco perez <frperez032219@gmail.com>
2023-03-29 20:28:04 -0700

    working only link to one app

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -10,10 +10,10 @@ const Projects = () => {
     <div id='projects' className='max-w-[1040px] m-auto md:pl-20 p-4 py-16'>
       <h1 className='text-4xl font-bold text-center'>Projects</h1>
       <p className='text-center py-8'>
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Earum ab eius dolore veritatis qui quasi, optio minima adipisci doloremque animi, natus aspernatur ipsam corporis id alias veniam? Corrupti, ab atque.
+        I take insperation from my daily life in order create projects that can help make daily life easier. Weather it be from my love of video games to raising a child with special needs. Everything and anything can be solved with a little passion and a lot of hours coding!
       </p>
       <div className='grid sm:grid-cols-2 gap-12'>
-        <ProjectItem  img={AmeliorateImg} title= 'Ameliorate App'/>
+        <ProjectItem  img={AmeliorateImg} title= 'Ameliorate App' link={'https://ameliorate.herokuapp.com/login'}/>
         <ProjectItem  img={NoteTakerImg} title= 'NoteTaker App'/>
         <ProjectItem  img={TextEditorImg} title= 'TextEditor App'/>
       </div>
